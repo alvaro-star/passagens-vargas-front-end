37f3a67c12362741a681ebb5a9e2f67c08bb1110
alvaro-star <vargasalvaro248@gmail.com>
2024-04-09 07:47:33 -0400

    v1.8 - Criação do componente FormInlineTemplate, o formulário em linha da página home

--- src/Pages/Publico/Components/FormInlineTemplate.tsx
+++ src/Pages/Publico/Components/FormInlineTemplate.tsx
@@ -1,18 +1,130 @@
-import React, { FormHTMLAttributes } from "react"
+import React, { FormHTMLAttributes, useEffect, useState } from "react"
+import InputError from "../../../Components/InputError"
+import TextInput from "../../../Components/TextInput"
+import ICiudad from "../../../Types/ICiudad"
+import { useNavigate } from "react-router-dom"
+import http from "../../../http"
+import IPage from "../../../Types/IPage"
+import ICampo from "../../../Types/ICampo"
 
 interface Props extends FormHTMLAttributes<HTMLFormElement> {
     className?: string,
-    children?: React.ReactNode
 }
 
-const FormTemplate = ({ children, className, ...props }: Props) => {
+
+
+const FormInlineTemplate = ({ className, ...props }: Props) => {
+
+    const [ciudadSalida, setCiudadSalida] = useState<ICampo<string>>({ value: '', erro: '' })
+    const [ciudadDestino, setCiudadDestino] = useState<ICampo<string>>({ value: '', erro: '' })
+    const [fechaIda, setFechaIda] = useState('')
+    const [fechaVuelta, setFechaVuelta] = useState('')
+
+    const [ciudadesSalida, setCiudadesSalida] = useState<ICiudad[]>([])
+    const [ciudadesDestino, setCiudadesDestino] = useState<ICiudad[]>([])
+
+    const navigate = useNavigate()
+    const enviar = (eve: React.FormEvent<HTMLFormElement>) => {
+        eve.preventDefault()
+        setCiudadSalida({ value: ciudadSalida.value, erro: '' })
+        setCiudadDestino({ value: ciudadDestino.value, erro: '' })
+        if (ciudadSalida === ciudadDestino) {
+            setCiudadSalida({ value: ciudadSalida.value, erro: 'La salida es igual al destino' })
+            setCiudadDestino({ value: ciudadDestino.value, erro: 'El destino es igual ala salida' })
+        }
+
+        let idSalida = -1
+        ciudadesSalida.forEach((ci) => {
+            if (ci.nombre === ciudadSalida.value) {
+                idSalida = ci.id
+            }
+        })
+
+        let idDestino = -1
+        ciudadesDestino.forEach((ci) => {
+            if (ci.nombre === ciudadDestino.value) {
+                idDestino = ci.id
+            }
+        })
+
+        if (idSalida == -1) {
+            setCiudadSalida({ value: ciudadSalida.value, erro: 'Escribe una ciudad válido' })
+        }
+
+        if (idDestino == -1) {
+            setCiudadDestino({ value: ciudadDestino.value, erro: 'Escribe una ciudad válido' })
+        }
+
+        if (idDestino != -1 && idSalida != -1 && fechaIda != '') {
+            const formViajes = {
+                idCiudadSalida: idSalida,
+                idCiudadDestino: idDestino,
+                fechaSalida: fechaIda,
+                fechaVuelta: fechaVuelta
+            }
+
+            sessionStorage.setItem("formViaje", JSON.stringify(formViajes))
+            navigate('/viajes')
+        }
+
+    }
+
+    useEffect(() => {
+        if (ciudadSalida.value != '') {
+            http.get<IPage<ICiudad>>(`ciudades/${ciudadSalida.value}/like`)
+                .then(resposta => {
+                    setCiudadesSalida(resposta.data.content)
+                })
+        }
+    }, [ciudadSalida])
+
+    useEffect(() => {
+        if (ciudadDestino.value != '') {
+            http.get<IPage<ICiudad>>(`ciudades/${ciudadDestino.value}/like`)
+                .then(resposta => {
+                    setCiudadesDestino(resposta.data.content)
+                })
+        }
+    }, [ciudadDestino])
     return (
-        <form
+        <form className={"max-w-5xl p-5 mx-auto shadow-xl bg-white flex items-center justify-between gap-5  rounded " + className}
             {...props}
-            className={`w-80 flex flex-col items-center justify-center bg-slate-200 p-5 rounded ${className}`}>
-            {children}
+            onSubmit={enviar}>
+            <div className="w-full">
+                <TextInput list="salidas" placeholder="Santa Cruz" value={ciudadSalida.value}
+                    onChange={eve => setCiudadSalida({ value: eve.target.value, erro: ciudadSalida.erro })}
+                    required />
+                <datalist id="salidas">
+                    {!(ciudadesSalida.length == 1 && ciudadesSalida[0].nombre === ciudadSalida.value) &&
+                        ciudadesSalida.map(
+                            ciudad => <option key={ciudad.id} value={ciudad.nombre}></option>)}
+                </datalist>
+                <InputError message={ciudadSalida.erro} />
+            </div>
+            <div className="w-full">
+                <TextInput list="destinos" placeholder="Cochabamba" value={ciudadDestino.value}
+                    onChange={eve => setCiudadDestino({ value: eve.target.value, erro: ciudadDestino.erro })}
+                    required />
+                <datalist id="destinos">
+                    {!(ciudadesDestino.length == 1 && ciudadesDestino[0].nombre === ciudadDestino.value) &&
+                        ciudadesDestino.map(
+                            ciudad => <option key={ciudad.id} value={ciudad.nombre}></option>)}
+                </datalist>
+                <InputError message={ciudadDestino.erro} />
+            </div>
+            <div>
+                <TextInput type="date" value={fechaIda} onChange={eve => setFechaIda(eve.target.value)} required />
+            </div>
+            <div>
+                <TextInput type="date" value={fechaVuelta} onChange={eve => setFechaVuelta(eve.target.value)} required />
+            </div>
+            <div className="flex items-center h-full justify-center">
+                <button className="h-9 w-9 rounded bg-cyan-500 text-white">
+                    B
+                </button>
+            </div>
         </form>
     )
 }
 
-export default FormTemplate
+export default FormInlineTemplate
--- src/Pages/Publico/Home.tsx
+++ src/Pages/Publico/Home.tsx
@@ -1,84 +1,9 @@
-import { useEffect, useState } from "react"
-import TextInput from "../../Components/TextInput"
-import http from "../../http"
-import IPage from "../../Types/IPage"
-import ICiudad from "../../Types/ICiudad"
-import InputError from "../../Components/InputError"
-import { Link, useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+import FormInlineTemplate from "./Components/FormInlineTemplate"
 
 const Home = () => {
-    const [ciudadSalida, setCiudadSalida] = useState('')
-    const [ciudadDestino, setCiudadDestino] = useState('')
-    const [fechaIda, setFechaIda] = useState('')
-    const [fechaVuelta, setFechaVuelta] = useState('')
-    const [ciudadesSalida, setCiudadesSalida] = useState<ICiudad[]>([])
-    const [ciudadesDestino, setCiudadesDestino] = useState<ICiudad[]>([])
-    const [salidaErro, setSalidaErro] = useState('')
-    const [destinoErro, setDestinoErro] = useState('')
 
-    const navigate = useNavigate()
-    const enviar = (eve: React.FormEvent<HTMLFormElement>) => {
-        eve.preventDefault()
-        setSalidaErro('')
-        setDestinoErro('')
-        if (ciudadSalida === ciudadDestino) {
-            setSalidaErro('La salida es igual al destino')
-            setDestinoErro('El destino es igual ala salida')
-        }
-
-        let idSalida = -1
-        ciudadesSalida.forEach((ci) => {
-            if (ci.nombre === ciudadSalida) {
-                idSalida = ci.id
-            }
-        })
-
-        let idDestino = -1
-        ciudadesDestino.forEach((ci) => {
-            if (ci.nombre === ciudadDestino) {
-                idDestino = ci.id
-            }
-        })
-
-        if (idSalida == -1) {
-            setSalidaErro('Escribe una ciudad válido')
-        }
-
-        if (idDestino == -1) {
-            setDestinoErro('Escribe una ciudad válido')
-        }
-
-        if (idDestino != -1 && idSalida != -1 && fechaIda != '') {
-            const formViajes = {
-                idCiudadSalida: idSalida,
-                idCiudadDestino: idDestino,
-                fechaSalida: fechaIda,
-                fechaVuelta: fechaVuelta
-            }
-
-            sessionStorage.setItem("formViaje", JSON.stringify(formViajes))
-            navigate('/viajes')
-        }
-
-    }
-
-    useEffect(() => {
-        if (ciudadSalida != '') {
-            http.get<IPage<ICiudad>>(`ciudades/${ciudadSalida}/like`)
-                .then(resposta => {
-                    setCiudadesSalida(resposta.data.content)
-                })
-        }
-    }, [ciudadSalida])
-
-    useEffect(() => {
-        if (ciudadDestino != '') {
-            http.get<IPage<ICiudad>>(`ciudades/${ciudadDestino}/like`)
-                .then(resposta => {
-                    setCiudadesDestino(resposta.data.content)
-                })
-        }
-    }, [ciudadDestino])
+    const token = sessionStorage.getItem('token')
 
     return (
         <div className="w-full relative flex items-center justify-center flex-col">
@@ -87,10 +12,16 @@ const Home = () => {
                     <div className="text-2xl font-semibold">
                         Logo
                     </div>
-                    <div className="flex items-center gap-4">
-                        <Link to='/registrar' className="rounded px-3 py-1.5 hover:bg-white hover:text-black">REGISTRAR</Link>
-                        <Link to='/login' className="rounded bg-white text-black px-3 py-1.5">ENTRAR</Link>
-                    </div>
+                    {!token ?
+                        <div className="flex items-center gap-4">
+                            <Link to='/registrar' className="rounded px-3 py-1.5 hover:bg-white hover:text-black">REGISTRAR</Link>
+                            <Link to='/login' className="rounded bg-white text-black px-3 py-1.5">ENTRAR</Link>
+                        </div>
+                        :
+                        <div className="flex items-center gap-4">
+                            <Link to='/login' className="rounded bg-white text-black px-3 py-1.5">Log Out</Link>
+                        </div>
+                    }
                 </div>
                 <header className="w-full text-white h-96 bg-gray-500 grid place-content-center">
                     <div className="text-center">
@@ -100,37 +31,7 @@ const Home = () => {
                     </div>
                 </header>
                 <section className="w-full h-16 -mt-9 absolute text-black bg-white bg-opacity-0">
-                    <form className="mx-auto bg-white flex items-center justify-between gap-5 shadow-xl max-w-5xl p-5 rounded " onSubmit={enviar}>
-                        <div className="w-full">
-                            <TextInput list="salidas" placeholder="Santa Cruz" value={ciudadSalida} onChange={eve => setCiudadSalida(eve.target.value)} required />
-                            <datalist id="salidas">
-                                {!(ciudadesSalida.length == 1 && ciudadesSalida[0].nombre === ciudadSalida) &&
-                                    ciudadesSalida.map(
-                                        ciudad => <option key={ciudad.id} value={ciudad.nombre}></option>)}
-                            </datalist>
-                            <InputError message={salidaErro} />
-                        </div>
-                        <div className="w-full">
-                            <TextInput list="destinos" placeholder="Cochabamba" value={ciudadDestino} onChange={eve => setCiudadDestino(eve.target.value)} required />
-                            <datalist id="destinos">
-                                {!(ciudadesDestino.length == 1 && ciudadesDestino[0].nombre === ciudadDestino) &&
-                                    ciudadesDestino.map(
-                                        ciudad => <option key={ciudad.id} value={ciudad.nombre}></option>)}
-                            </datalist>
-                            <InputError message={destinoErro} />
-                        </div>
-                        <div>
-                            <TextInput type="date" value={fechaIda} onChange={eve => setFechaIda(eve.target.value)} required />
-                        </div>
-                        <div>
-                            <TextInput type="date" value={fechaVuelta} onChange={eve => setFechaVuelta(eve.target.value)} required />
-                        </div>
-                        <div className="flex items-center h-full justify-center">
-                            <button className="h-9 w-9 rounded bg-cyan-500 text-white">
-                                B
-                            </button>
-                        </div>
-                    </form>
+                    <FormInlineTemplate />
                 </section>
 
                 <section className="w-full mt-24 mx-24">
